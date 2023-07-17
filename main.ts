62acfcb74b2b0c8f444d63abbae8c015eac901af
aaf-30 <81393721+aaf-30@users.noreply.github.com>
2023-07-17 23:02:01 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import express from "https://esm.sh/express@4.18.2";
 const app = express();
 
 app.get("/", function (req, res) {
-    res.send(req.query.id)
+    // res.send(req.query.id)
     res.redirect("tel:"+req.query.id);
 });
 
