7248d9dd5323f2ffccc69b90fed04daa7ab2e4c9
Bmgpt <137195478+Bmgpt@users.noreply.github.com>
2023-09-26 23:32:17 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ const app = express();
 
 app.get("/", function (req, res) {
     if(req.query.number){
-      res.redirect("tel:*31*+"+req.query.number);
+      res.redirect("tel:*23#+"+req.query.number);
     }else{
       res.send("Invalid")
     }
